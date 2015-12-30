c228ad44dfbd40a892914ab0fb84b88a4db56049
eggiter <haodonglyu@gmail.com>
2015-12-30 09:15:24 +0800

    update data

--- cdnmf/data/result/plot.py
+++ cdnmf/data/result/plot.py
@@ -11,15 +11,21 @@ def plot():
                  'email/nmi/I2lmd1.txt', \
                  'email/nmi/I2lmd5.txt', \
                  'email/nmi/I2lmd10.txt']
+    list_file = ['email/nmi/I2lmd0.txt',\
+                 'email/nmi/I2lmd0.0.txt']
     list_data = []
     for f in list_file:
         list_data.append(loadlist(f))
     x = range(1, len(list_data[0])+1)
+    pl.plot(x, list_data[0], '-r', label='lmd=0')
+    pl.plot(x, list_data[1], '-g', label='lmd=0.0')
+    '''
     pl.plot(x, list_data[0], '-r', label='lmd=0.0')
-    #pl.plot(x, list_data[1], '-g', label='lmd=0.5')
-    #pl.plot(x, list_data[2], '-b', label='lmd=1.0')
+    pl.plot(x, list_data[1], '-g', label='lmd=0.5')
+    pl.plot(x, list_data[2], '-b', label='lmd=1.0')
     pl.plot(x, list_data[3], '-m', label='lmd=5')
     pl.plot(x, list_data[4], '-k', label='lmd=10')
+    '''
     pl.xlabel('Time Slice')
     pl.ylabel('NMI')
     pl.legend(loc='lower right')
